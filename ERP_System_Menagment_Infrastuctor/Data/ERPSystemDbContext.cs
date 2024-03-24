1e1acc2f11696f39f5c3bfa0c10ef0b7a2344d14
radoslav hristov <bmwto@outlook.com>
2024-03-24 16:10:40 +0200

    Add project files.

---
+++ ERP_System_Menagment_Infrastuctor/Data/ERPSystemDbContext.cs
@@ -0,0 +1,17 @@
+﻿using ERP_System_Menagment_Infrastuctor.Data.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace ERP_System_Menagment_Infrastuctor.Data
+{
+    
+    public class ERPSystemDbContext : IdentityDbContext<Users>
+    {
+        public ERPSystemDbContext(DbContextOptions<ERPSystemDbContext> option)
+            : base(option)
+        {
+        }
+
+        public DbSet<Files> DataFiles { get; set; } = null!;
+    }
+}
---
+++ ERP_System_Menagment_Infrastuctor/Data/Models/Users.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace ERP_System_Menagment_Infrastuctor.Data.Models
+{
+    public class Users : IdentityUser
+    {
+        public string FirstName { get; set; } = null!;
+
+        public string LastName { get; set; } = null!;
+
+        public List<Files> UserFiles { get; set; } = new List<Files>();
+    }
+}
