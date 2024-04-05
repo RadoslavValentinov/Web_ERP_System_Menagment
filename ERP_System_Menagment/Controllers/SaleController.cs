395032201521760d842e48f3aabd6f47a564dda4
radoslav hristov <bmwto@outlook.com>
2024-04-05 19:18:16 +0300

    # new priooperty to product class and controller sale and class ##

---
+++ ERP_System_Menagment/Controllers/SaleController.cs
@@ -0,0 +1,29 @@
+﻿using ERP_System_Menagment_Core.ModelView.SaleModels;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ERP_System_Menagment.Controllers
+{
+    [Authorize]
+    public class SaleController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        [HttpGet]
+        public IActionResult CreateSale()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult CreateSale(CreateSaleViewModel model)
+        {
+            return View(model);
+        }
+
+    }
+}
