e94c3c3037003cdc03a821021df59f3f77232a03
radoslav hristov <bmwto@outlook.com>
2024-04-13 20:54:07 +0300

    ## Create database and class,Controller and view sal######e #

--- ERP_System_Menagment/Program.cs
+++ ERP_System_Menagment/Program.cs
@@ -7,7 +7,7 @@ namespace ERP_System_Menagment
 {
     public class Program
     {
-        public static async Task Main(string[] args)
+        public static async  Task Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
 
@@ -65,86 +65,86 @@ namespace ERP_System_Menagment
                 pattern: "{controller=Home}/{action=Index}/{id?}");
 
 
-            //using (var scope = app.Services.CreateScope())
-            //{
-            //    var roleManager = scope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+            using (var scope = app.Services.CreateScope())
+            {
+                var roleManager = scope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
 
-            //    var roles = new[] { "Administrator", "User", "Menager" };
+                var roles = new[] { "Administrator", "User", "Menager" };
 
-            //    foreach (var role in roles)
-            //    {
-            //        if (!await roleManager.RoleExistsAsync(role))
-            //            await roleManager.CreateAsync(new IdentityRole(role));
-            //    }
-            //}
+                foreach (var role in roles)
+                {
+                    if (!await roleManager.RoleExistsAsync(role))
+                        await roleManager.CreateAsync(new IdentityRole(role));
+                }
+            }
 
-            //using (var scope = app.Services.CreateScope())
-            //{
-            //    var userManager = scope.ServiceProvider.GetRequiredService<UserManager<Users>>();
+            using (var scope = app.Services.CreateScope())
+            {
+                var userManager = scope.ServiceProvider.GetRequiredService<UserManager<Users>>();
 
-            //    string email = "Repoit@abv.bg";
+                string email = "Repoit@abv.bg";
 
-            //    if (await userManager.FindByEmailAsync(email) == null)
-            //    {
-            //        var user = new Users()
-            //        {
-            //            Id = "b5a3dd97-a911-493b-88c0-cd062aef88bc",
-            //            Avatar = "https://us.123rf.com/450wm/anatolir/anatolir2011/anatolir201105528/159470802-jurist-avatar-icon-flat-style.jpg?ver=6",
-            //            FirstName = "Radoslav",
-            //            LastName = "Hristov",
-            //            Email = "Repoit@abv.bg",
-            //            EmailConfirmed = true,
-            //            UserName = "Admin",
-            //            NormalizedUserName = "Admin",
-            //            NormalizedEmail = "Repoit@abv.bg",
-            //        };
+                if (await userManager.FindByEmailAsync(email) == null)
+                {
+                    var user = new Users()
+                    {
+                        Id = "b5a3dd97-a911-493b-88c0-cd062aef88bc",
+                        Avatar = "https://us.123rf.com/450wm/anatolir/anatolir2011/anatolir201105528/159470802-jurist-avatar-icon-flat-style.jpg?ver=6",
+                        FirstName = "Radoslav",
+                        LastName = "Hristov",
+                        Email = "Repoit@abv.bg",
+                        EmailConfirmed = true,
+                        UserName = "Admin",
+                        NormalizedUserName = "Admin",
+                        NormalizedEmail = "Repoit@abv.bg",
+                    };
 
-            //        await userManager.CreateAsync(user, "Admin3516@");
-            //        await userManager.AddToRoleAsync(user, "Administrator");
-            //    }
+                    await userManager.CreateAsync(user, "Admin3516@");
+                    await userManager.AddToRoleAsync(user, "Administrator");
+                }
 
-            //    string menEmail = "maria89@gmail.com";
+                string menEmail = "maria89@gmail.com";
 
-            //    if (await userManager.FindByEmailAsync(menEmail) == null)
-            //    {
-            //        var menagerUser = new Users()
-            //        {
-            //            Id = "e8d663de-89f9-40fb-a7a3-ec0f66a18aa5",
-            //            Avatar = "https://cdn2.vectorstock.com/i/1000x1000/54/41/young-and-elegant-woman-avatar-profile-vector-9685441.jpg",
-            //            FirstName = "Maria",
-            //            LastName = "Atanasova",
-            //            Email = "maria89@gmail.com",
-            //            EmailConfirmed = true,
-            //            UserName = "maria89@gmail.com",
-            //            NormalizedEmail = "maria89@gmail.com",
-            //            NormalizedUserName = "maria89@gmail.com"
-            //        };
+                if (await userManager.FindByEmailAsync(menEmail) == null)
+                {
+                    var menagerUser = new Users()
+                    {
+                        Id = "e8d663de-89f9-40fb-a7a3-ec0f66a18aa5",
+                        Avatar = "https://cdn2.vectorstock.com/i/1000x1000/54/41/young-and-elegant-woman-avatar-profile-vector-9685441.jpg",
+                        FirstName = "Maria",
+                        LastName = "Atanasova",
+                        Email = "maria89@gmail.com",
+                        EmailConfirmed = true,
+                        UserName = "maria89@gmail.com",
+                        NormalizedEmail = "maria89@gmail.com",
+                        NormalizedUserName = "maria89@gmail.com"
+                    };
 
-            //        await userManager.CreateAsync(menagerUser, "Maria123@");
-            //        await userManager.AddToRoleAsync(menagerUser, "Menager");
-            //    }
+                    await userManager.CreateAsync(menagerUser, "Maria123@");
+                    await userManager.AddToRoleAsync(menagerUser, "Menager");
+                }
 
-            //    string userEmail = "bobi1101@gmail.com";
+                string userEmail = "bobi1101@gmail.com";
 
-            //    if (await userManager.FindByEmailAsync(userEmail) == null)
-            //    {
-            //        var menagerUser = new Users()
-            //        {
-            //            Id = "f620aaf2-03e7-4252-adf8-bc286f246a45",
-            //            Avatar = "https://us.123rf.com/450wm/anatolir/anatolir2011/anatolir201105528/159470802-jurist-avatar-icon-flat-style.jpg?ver=6",
-            //            FirstName = "Borislav",
-            //            LastName = "Antonov",
-            //            Email = "bobi1101@gmail.com",
-            //            EmailConfirmed = true,
-            //            UserName = "bobi1101@gmail.com",
-            //            NormalizedEmail = "bobi1101@gmail.com",
-            //            NormalizedUserName = "bobi1101@gmail.com"
-            //        };
+                if (await userManager.FindByEmailAsync(userEmail) == null)
+                {
+                    var menagerUser = new Users()
+                    {
+                        Id = "f620aaf2-03e7-4252-adf8-bc286f246a45",
+                        Avatar = "https://us.123rf.com/450wm/anatolir/anatolir2011/anatolir201105528/159470802-jurist-avatar-icon-flat-style.jpg?ver=6",
+                        FirstName = "Borislav",
+                        LastName = "Antonov",
+                        Email = "bobi1101@gmail.com",
+                        EmailConfirmed = true,
+                        UserName = "bobi1101@gmail.com",
+                        NormalizedEmail = "bobi1101@gmail.com",
+                        NormalizedUserName = "bobi1101@gmail.com"
+                    };
 
-            //        await userManager.CreateAsync(menagerUser, "Bobi31126@");
-            //        await userManager.AddToRoleAsync(menagerUser, "User");
-            //    }
-            //}
+                    await userManager.CreateAsync(menagerUser, "Bobi31126@");
+                    await userManager.AddToRoleAsync(menagerUser, "User");
+                }
+            }
 
 
             app.Run();
