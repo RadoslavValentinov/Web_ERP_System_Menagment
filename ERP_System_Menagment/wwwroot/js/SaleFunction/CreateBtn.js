3d3be1f83dd49e9a4c61e7cd2caddf41c94f5962
radoslav hristov <bmwto@outlook.com>
2024-04-20 19:33:50 +0300

    ## Create button to allProduct change  ###

--- ERP_System_Menagment/wwwroot/js/SaleFunction/CreateBtn.js
+++ ERP_System_Menagment/wwwroot/js/SaleFunction/CreateBtn.js
@@ -2,5 +2,9 @@
 
     let getBtn = document.getElementById("getAll");
 
+<<<<<<< HEAD
+    // getBtn.style.visibility = 'visible';
+=======
+>>>>>>> ec55b1c22bee350e21e6a8958929dc6f0dcd5fe7
     getBtn.removeAttribute('hidden');
 }
--- ERP_System_Menagment/wwwroot/js/SaleFunction/NewLine.js
+++ ERP_System_Menagment/wwwroot/js/SaleFunction/NewLine.js
@@ -1,6 +1,11 @@
 ﻿function newLine() {
 
     let getTable = document.getElementById("AllInfo");
+<<<<<<< HEAD
+    let getInput = document.getElementById("first");
+    let ch = getTable.children;
+=======
+>>>>>>> ec55b1c22bee350e21e6a8958929dc6f0dcd5fe7
     let ch = getTable.children;
 
 
@@ -9,6 +14,18 @@
     let tdFirst = document.createElement('td');
     let inputFirst = document.createElement('input');
     inputFirst.type = 'text';
+<<<<<<< HEAD
+    inputFirst.setAttribute('id', 'first');
+    inputFirst.addEventListener('focus', seeBtn);
+
+    let btn = document.createElement('button');
+    btn.textContent = '...';
+    btn.setAttribute('id', 'getAll');
+    btn.style.visibility = 'hidden';
+    btn.addEventListener('click', AllProd);
+
+=======
+>>>>>>> ec55b1c22bee350e21e6a8958929dc6f0dcd5fe7
     inputFirst.addEventListener('focus', seeBtn);
 
     let btn = document.createElement('button');
@@ -19,6 +36,11 @@
 
     tdFirst.appendChild(inputFirst);
     tdFirst.appendChild(btn);
+<<<<<<< HEAD
+    tdFirst.appendChild(btn);
+
+=======
+>>>>>>> ec55b1c22bee350e21e6a8958929dc6f0dcd5fe7
 
     let tdSecond = document.createElement('td');
     let inputSecond = document.createElement('input');
@@ -29,12 +51,12 @@
     let inputThree = document.createElement('input');
     inputThree.type = 'text';
     tdThree.appendChild(inputThree);
-   
+
 
     let tdFour = document.createElement('td');
     let inputFour = document.createElement('input');
     inputFour.type = 'text';
-    inputFour.addEventListener('keypress',newLine)
+    inputFour.addEventListener('keypress', newLine);
     tdFour.appendChild(inputFour);
 
 
