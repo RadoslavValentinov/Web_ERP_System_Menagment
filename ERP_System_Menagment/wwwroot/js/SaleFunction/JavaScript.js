7a64a7b7dad90a9251543b2f38b1af1316b4c206
radoslav hristov <bmwto@outlook.com>
2024-05-06 08:55:21 +0300

    ## Add new test function JS toautocomplete field and css style to layout page. ##

---
+++ ERP_System_Menagment/wwwroot/js/SaleFunction/JavaScript.js
@@ -0,0 +1,17 @@
+﻿$(document).ready(function () {
+    $("#first").autocomplete({
+        source: function (request, response) {
+            $.ajax({
+                url: "/Home/GetRecord",
+                type: "POST",
+                dataType: "json",
+                data: { prefix: request.term },
+                success: function (data) {
+                    response($.map(data, function (item) {
+                        return console.log(item);
+                    }))
+                }
+            })
+        },
+    });
+});
