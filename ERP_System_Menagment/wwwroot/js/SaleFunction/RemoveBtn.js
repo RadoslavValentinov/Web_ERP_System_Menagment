1321ade55c79dfdd3d4144630ede582bc0c77b60
radoslav hristov <bmwto@outlook.com>
2024-06-04 21:14:15 +0300

    ############

--- ERP_System_Menagment/wwwroot/js/SaleFunction/RemoveBtn.js
+++ ERP_System_Menagment/wwwroot/js/SaleFunction/RemoveBtn.js
@@ -6,9 +6,21 @@
 
     getDivHide.style.visibility = 'hidden';
     getBtn.style.display = 'none';
+
+
+    getDivHide.style.visibility = 'hidden';
+    getBtn.style.display = 'none';
+=======
+<<<<<<< HEAD
+    let getBtn = document.getElementById("getAll");
+>>>>>>> fcd80a6337a043c730cc7cd232122d24fbe9db3e
+
+    getDivHide.style.visibility = 'hidden';
+    getBtn.style.display = 'none';
 =======
 
     getDivHide.style.visibility = 'hidden';
 >>>>>>> 27a318b20d878b3fba016a17043018e56fdd201a
+
 
 }
