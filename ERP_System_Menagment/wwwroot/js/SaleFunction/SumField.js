42cf03f575030677a8380c15ddc84047d8da9477
radoslav hristov <bmwto@outlook.com>
2024-06-05 20:14:02 +0300

    ## Test function to autocomplete field and set price in field of curent product. ##

---
+++ ERP_System_Menagment/wwwroot/js/SaleFunction/SumField.js
@@ -0,0 +1,10 @@
+﻿
+function resultSum() {
+
+    let getQuantity = document.getElementById("quantity");
+    let getPrice = document.getElementById("price");
+    let getSum = document.getElementById("Sum");
+
+    getSum.value = Number(getQuantity.value * getPrice.value);
+
+}
