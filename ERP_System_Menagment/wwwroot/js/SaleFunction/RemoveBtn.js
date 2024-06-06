7b10faeb04781257c5491248ee273f5087559e68
radoslav hristov <bmwto@outlook.com>
2024-06-06 20:43:22 +0300

    ## Test function to autocomplete field and set price and Sum field##

--- ERP_System_Menagment/wwwroot/js/SaleFunction/RemoveBtn.js
+++ ERP_System_Menagment/wwwroot/js/SaleFunction/RemoveBtn.js
@@ -1,26 +1,9 @@
 ﻿function removeBtn() {
 
     let getDivHide = document.getElementById("result");
-<<<<<<< HEAD
     let getBtn = document.getElementById("getAll");
 
     getDivHide.style.visibility = 'hidden';
     getBtn.style.display = 'none';
-
-
-    getDivHide.style.visibility = 'hidden';
-    getBtn.style.display = 'none';
-=======
-<<<<<<< HEAD
-    let getBtn = document.getElementById("getAll");
->>>>>>> fcd80a6337a043c730cc7cd232122d24fbe9db3e
-
-    getDivHide.style.visibility = 'hidden';
-    getBtn.style.display = 'none';
-=======
-
-    getDivHide.style.visibility = 'hidden';
->>>>>>> 27a318b20d878b3fba016a17043018e56fdd201a
-
 
 }
